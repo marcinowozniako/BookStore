948d9217d56f33a362c91bacf89c9c14bb329c12
marcinowozniako <marekboniek@gmail.com>
2022-05-17 20:31:45 +0200

    models, migrations done

--- books/models.py
+++ books/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    books = models.ManyToManyField('Book', related_name='authors', blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    books = models.ManyToManyField('Book', related_name='categories', blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    book_id = models.CharField(max_length=255, unique=True)
+    title = models.CharField(max_length=255)
+    published_date = models.CharField(max_length=12)
+    acquired = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
